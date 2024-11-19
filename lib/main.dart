aa30c03cc17ecd1bfec3bf46d79203ec5ce843fc
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 19:29:28 +0900

    地図 - 各種マーカー - MarkerLayer

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,31 @@ void main() {
         urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // 6
         userAgentPackageName: 'com.example.app', // 7
       ),
-      const SimpleAttributionWidget(
-        source: Text('OpenStreetMap contributors'),
+      const MarkerLayer(
+        markers: [         
+          Marker(
+            width: 20.0,
+            height: 20.0,
+            point: LatLng(35.67604049, 139.74527642), // ピンの位置
+            child: Icon( // アイコンを使う例
+              Icons.location_on,
+              color: Colors.red, // ピンの色
+              size: 30, // ピンのサイズ
+            ),
+            rotate: true, // マーカーまで回転しないようにする場合  
+         ),
+        ],
       ),
-      // RichAttributionWidget(
-      //   attributions: [
-      //     // OpenStreetMapのクレジット表記
-      //     TextSourceAttribution(
-      //       'OpenStreetMap contributors',
-      //       onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-      //     ),
-      //     // その他、必要に応じて表記やリンク等を追加していく事。
-      //   ],
-      // ),
+      RichAttributionWidget( // 8
+        attributions: [
+          // OpenStreetMapのクレジット表記
+          TextSourceAttribution( // 9
+            'OpenStreetMap contributors',
+            onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+          ),
+          // その他、必要に応じて表記やリンク等を追加していく事。
+        ],
+      ),
     ],
   );
   final body = fm;  
